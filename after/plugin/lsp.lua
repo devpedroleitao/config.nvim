8a6b517720d2418630e571f76e9cfb2cf3438fca
Pedro Leitao <dev.pedro.leitao@gmail.com>
2024-05-06 11:21:47 +0100

    added config for ansible

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -97,6 +97,30 @@ mason_lspconfig.setup({
                 filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx', 'css', 'scss', 'angular.html' },
             })
         end,
+        ansiblels = function ()
+            lspconfig.ansiblels.setup({
+                settings = {
+                    ansible = {
+                        ansible = {
+                            path = "ansible"
+                        },
+                        executionEnvironment = {
+                            enabled = false
+                        },
+                        python = {
+                            interpreterPath = "python"
+                        },
+                        validation = {
+                            enabled = true,
+                            lint = {
+                                enabled = true,
+                                path = "ansible-lint"
+                            }
+                        }
+                    }
+                }
+            })
+        end,
 
     },
 })
