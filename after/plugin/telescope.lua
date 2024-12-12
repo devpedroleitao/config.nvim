7c29319ed743f04fd1f6e1a7d8dc8a1dce7aaf86
devpedroleitao <dev.pedro.leitao@gmail.com>
2024-12-12 14:59:00 +0000

    Use find_files when not in a git repo.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,11 +4,19 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pfh', function()
     builtin.find_files({ hidden = true, no_ignore = true })
 end, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>',
+    function ()
+        local ok = pcall(builtin.git_files)
+        if not ok then
+            builtin.find_files()
+        end
+    end
+    , {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>psh', '<CMD>Telescope live_grep vimgrep_arguments=rg,--color=never,--no-heading,--no-heading,--line-number,--column,--smart-case,--hidden,--no-ignore<CR>')
+vim.keymap.set('n', '<leader>psh',
+    '<CMD>Telescope live_grep vimgrep_arguments=rg,--color=never,--no-heading,--no-heading,--line-number,--column,--smart-case,--hidden,--no-ignore<CR>')
 
 -- vim.keymap.set('n', '<leader>psh', function()
 --     builtin.grep_string({ search = vim.fn.input("Grep Hidden> "), additional_args = { "--hidden" } });
